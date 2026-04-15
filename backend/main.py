4a030e677193b9703794b6909f2d881bfae60f17
EdgarIvan01 <edgarivan.lozada.s@gmail.com>
2026-04-15 11:10:26 -0600

    fix error bert deploy

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,8 @@ app = FastAPI(title="RealitiAI Backend", version="1.0.0")
 # ══════════════════════════════════════
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    #allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
